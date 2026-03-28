cd58d0db82e16aa11d2e30bf91eb2a76374b3dd7
Jesus Michael <official1.jesusmichael@gmail.com>
2026-03-28 15:24:48 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "solana-shield-ai-1778737-ce957",
+  "appId": "1:541172212305:web:6b08255003af03d4d34e70",
+  "apiKey": "AIzaSyAk-Nu1_-TBD6igRQq9hvLTJGhQ8BaOnDM",
+  "authDomain": "solana-shield-ai-1778737-ce957.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "541172212305"
+};
